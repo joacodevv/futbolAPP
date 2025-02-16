5556ecf8cd72128a9148d729615a97b6b3fb8d7f
joacoorsini <joaquinorsi2008@gmail.com>
2025-02-15 21:00:57 -0300

    level base

--- app/src/main/java/com/example/adad/MainActivity.kt
+++ app/src/main/java/com/example/adad/MainActivity.kt
@@ -7,11 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -20,43 +21,121 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.adad.ui.theme.AdadTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-                Screen(questionViewModel = QuestionViewModel())
+            Screen(questionViewModel = QuestionViewModel())
         }
     }
+
+
 }
 
 @Composable
-fun Screen(questionViewModel: QuestionViewModel ){
+fun Screen(questionViewModel: QuestionViewModel) {
 
     val questionData = questionViewModel.questionsData.collectAsState()
     var isVisible by remember { mutableStateOf(false) }
 
+    var answer1 by remember { mutableStateOf("") }
+    var answer2 by remember { mutableStateOf("") }
+    var answer3 by remember { mutableStateOf("") }
+    var answer4 by remember { mutableStateOf("") }
+    var answer5 by remember { mutableStateOf("") }
 
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 28.dp),
+        contentAlignment = Alignment.TopCenter
+    ) {
+
+        Column {
             Button(onClick = {
                 questionViewModel.fetchQuestions()
                 isVisible = true
+                Log.i("Questions", questionViewModel.questionsData.value.toString())
             }) {
-                Text(text = "Questions")
-            }
-            AnimatedVisibility(visible = isVisible) {
-                questionData.value?.let { Text(it.pregunta) }
+                Text(text = "Start Game")
             }
 
+            AnimatedVisibility(visible = isVisible) {
+                Column {
+                    Text(text = questionData.value?.pregunta ?: "")
+
+                    TextField(value = answer1,
+                        onValueChange = { answer1 = it },
+                        label = { Text("Answer") })
+                    TextField(value = answer2,
+                        onValueChange = { answer2 = it },
+                        label = { Text("Answer") })
+                    TextField(value = answer3,
+                        onValueChange = { answer3 = it },
+                        label = { Text("Answer") })
+                    TextField(value = answer4,
+                        onValueChange = { answer4 = it },
+                        label = { Text("Answer") })
+                    TextField(value = answer5,
+                        onValueChange = { answer5 = it },
+                        label = { Text("Answer") })
+                    Button(onClick = {
+
+                        Log.i(
+                            "Answers",
+                            questionViewModel.checkAnswer1(answer1)
+                                .toString() + questionViewModel.checkAnswer2(answer2)
+                                .toString() + questionViewModel.checkAnswer3(answer3)
+                                .toString() + questionViewModel.checkAnswer4(answer4)
+                                .toString() + questionViewModel.checkAnswer5(answer5)
+                        )
+
+                        if (questionViewModel.checkWin(
+                                questionViewModel.checkAnswer1(answer1),
+                                questionViewModel.checkAnswer2(answer2),
+                                questionViewModel.checkAnswer3(answer3),
+                                questionViewModel.checkAnswer4(answer4),
+                                questionViewModel.checkAnswer5(answer5)
+                            )
+                        ) {
+                            questionViewModel.fetchQuestions()
+                            answer1 = ""
+                            answer2 = ""
+                            answer3 = ""
+                            answer4 = ""
+                            answer5 = ""
+                        }
+
+
+                    }) {
+                        Text(text = "Submit")
+                    }
+                }
+            }
         }
+
+
+        // AnimatedVisibility(visible = isVisible) {
+        //     Column {
+        //         questionData.value?.let { Text(it.pregunta) }
+        //          questionData.value?.let {
+        //              it.respuestas.forEach {
+        //                  Text(text = it)
+        //             }
+        //        }
+        //     }
+        //}
 
     }
 
-fun callQuestions(viewModel: QuestionViewModel){
+}
+
+fun callQuestions(viewModel: QuestionViewModel) {
     viewModel.fetchQuestions()
     Log.i("Questions", viewModel.questionsData.value.toString())
 }
--- app/src/main/java/com/example/adad/ViewModel.kt
+++ app/src/main/java/com/example/adad/ViewModel.kt
@@ -21,25 +21,61 @@ class QuestionViewModel : ViewModel() {
 
     fun fetchQuestions() {
         viewModelScope.launch {
-            try{
+            try {
                 val num = ranNum()
                 val response = questionApi.getQuestions()
                 _questionsData.value = response.record.preguntas[num]
                 usedNumbers.add(num)
-            }catch (e: Exception){
+            } catch (e: Exception) {
                 Log.i("quepaso", _questionsData.value.toString())
             }
         }
     }
 
-    private fun ranNum(): Int{
-        val random = Random.nextInt(1,12)
+    private fun ranNum(): Int {
+        val random = Random.nextInt(1, 12)
 
-        return if (random in usedNumbers){
+        return if (random in usedNumbers) {
             ranNum()
-        }else{
+        } else {
             random
         }
     }
+
+    fun checkAnswer1(answer1: String): Boolean{
+        return answer1 == questionsData.value?.respuestas?.get(0)
+    }
+
+    fun checkAnswer2(answer2: String): Boolean{
+        return answer2 == questionsData.value?.respuestas?.get(1)
+    }
+
+    fun checkAnswer3(answer3: String): Boolean{
+        return answer3 == questionsData.value?.respuestas?.get(2)
+
+    }
+
+    fun checkAnswer4(answer4: String): Boolean{
+        return answer4 == questionsData.value?.respuestas?.get(3)
+
+    }
+
+    fun checkAnswer5(answer5: String): Boolean{
+        return answer5 == questionsData.value?.respuestas?.get(4)
+
+    }
+
+    fun checkWin(answer1: Boolean, answer2: Boolean, answer3: Boolean, answer4: Boolean, answer5: Boolean): Boolean{
+        return if(answer1 && answer2 && answer3 && answer4 && answer5){
+            true
+        }else{
+            false
+
+        }
+    }
+
+
+
+
 
 }
